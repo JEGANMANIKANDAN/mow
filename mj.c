51c0954378dd2dbb0a8e6a043d830571aaf2351d
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-02-08 16:41:50 +0530

    Create mj.c

---
+++ mj.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+clrscr();
+printf("Enter the a value ");
+scanf("%d",&a);
+printf("Enter the b value");
+scanf("%d",&b);
+
+for(i=a+1;i<b;i++)
+{
+    if(a%2!=0)
+    {
+        printf("%d");
+    }
+}
+getch();
+}
