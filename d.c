873bdf11cb993541c013a2263e6f75a482502f7f
JEGANMANIKANDAN <35652628+JEGANMANIKANDAN@users.noreply.github.com>
2018-02-08 15:54:18 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+   {
+
+    int a,i,count=0;
+    clrscr();
+    printf("Enter the number");
+for(i=1;i<=a;i++)
+{
+  if(a%2==0) 
+   {
+   count++;
+   }
+   
+   if(count==0)
+   {
+   printf("Yes");
+   }
+   else
+   {
+   printf("No");
+   }
+   getch();
+   }
+   
+  
+   
